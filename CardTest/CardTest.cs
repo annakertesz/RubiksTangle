5b26de8703b48a3ec6e2c19b217e041ba6d5be09
annakertesz <kerteszannanak@gmail.com>
2017-06-06 00:51:17 +0200

    CardTest added

---
+++ CardTest/CardTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RubiksTangle;
+
+namespace CardTest
+{
+    [TestClass]
+    public class CardTest
+    {
+        [TestMethod]
+        public void TestSetEdgesToZeroPosition()
+        {
+            Card card = new Card(new Color[] { Color.B, Color.Y, Color.R, Color.G, Color.B, Color.G, Color.Y, Color.R }, "firstA");
+            card.ActualPosition = 0;
+            Color[,] actual = card.ActualEdges;
+            Color[,] expected = new Color[4, 2] { { Color.B, Color.Y, }, { Color.R, Color.G }, { Color.B, Color.G }, { Color.Y, Color.R } };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSetEdgesToSecondPosition()
+        {
+            Card card = new Card(new Color[] { Color.B, Color.Y, Color.R, Color.G, Color.B, Color.G, Color.Y, Color.R }, "firstA");
+            card.ActualPosition = 2;
+            Color[,] actual = card.ActualEdges;
+            Color[,] expected = new Color[4, 2] { { Color.B, Color.G }, { Color.Y, Color.R }, { Color.B, Color.Y, }, { Color.R, Color.G } };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSetEdgesToPositionFour()
+        {
+            Card card = new Card(new Color[] { Color.B, Color.Y, Color.R, Color.G, Color.B, Color.G, Color.Y, Color.R }, "firstA");
+            card.ActualPosition = 4;
+            Color[,] actual = card.ActualEdges;
+            Color[,] expected = new Color[4, 2] { { Color.B, Color.Y, }, { Color.R, Color.G }, { Color.B, Color.G }, { Color.Y, Color.R } };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSetEdgesToPositionSix()
+        {
+            Card card = new Card(new Color[] { Color.B, Color.Y, Color.R, Color.G, Color.B, Color.G, Color.Y, Color.R }, "firstA");
+            card.ActualPosition = 6;
+            Color[,] actual = card.ActualEdges;
+            Color[,] expected = new Color[4, 2] { { Color.B, Color.G }, { Color.Y, Color.R }, { Color.B, Color.Y, }, { Color.R, Color.G } };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSetEdgesJoinableNotJoinable()
+        {
+            Card cardA = new Card(new Color[] { Color.B, Color.Y, Color.R, Color.G, Color.B, Color.G, Color.Y, Color.R }, "firstA");
+            Color[] edgeToJoin = new Color[] { Color.B, Color.G };
+            Assert.AreEqual(-1, cardA.isEdgesJoinable(edgeToJoin, 2));
+        }
+
+        [TestMethod]
+        public void TestSetEdgesJoinableIsJoinable()
+        {
+            Card cardA = new Card(new Color[] { Color.B, Color.Y, Color.R, Color.G, Color.B, Color.G, Color.Y, Color.R }, "firstA");
+            Color[] edgeToJoin = new Color[] { Color.G, Color.B };
+            Assert.AreEqual(2, cardA.isEdgesJoinable(edgeToJoin, 0));
+        }
+    }
+}
---
+++ CardTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CardTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CardTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94444f8b-0a4f-4fb3-a778-f9d6dd8f033b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
